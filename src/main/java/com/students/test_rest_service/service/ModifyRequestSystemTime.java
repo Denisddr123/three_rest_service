12429f68495e168564d1f51b271df0a89c66b48f
Denisddr123 <denddr@mail.ru>
2022-10-21 19:54:26 +0500

    Laba3 make ModifyRequestService

--- src/main/java/com/students/test_rest_service/service/ModifyRequestSystemTime.java
+++ src/main/java/com/students/test_rest_service/service/ModifyRequestSystemTime.java
@@ -7,10 +7,14 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Calendar;
+import java.util.Date;
+
 @Service
 public class ModifyRequestSystemTime implements  ModifyRequestService{
     public void modifyRq(Request request) {
-        request.setSystemTime("test");
+        Date date = new Date();
+        request.setSystemTime(date.toString());
         HttpEntity<Request> httpEntity = new HttpEntity<>(request);
         new RestTemplate().exchange("http://localhost:8082/feedback",
                 HttpMethod.POST,
